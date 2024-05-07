8c6c93ff6484568026f509e0a950c2abfffa3d0b
AlexanderCo787 <167447274+AlexanderCo787@users.noreply.github.com>
2024-05-07 15:55:14 +0300

    Я загрузил проект "Sum"
    Проект "Sum"

---
+++ sum/sum/sum.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    cout << "введите число 1: ";
+    int n;
+    cin >> n;
+    cout << "введите число 2: ";
+    int n1;
+    cin >> n1;
+
+    n = n + n1;
+
+    cout << "сумма двух чисел: " << n;
+    return 0;
+}
+
+
